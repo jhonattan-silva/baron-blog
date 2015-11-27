3f0466827ca0cf743f1eca59ce87da1293a38001
kreac <jhonattanwcs@hotmail.com>
2015-11-27 02:30:33 -0200

    Ajuste no sidebar e add categorias

---
+++ category-filmes.php
@@ -0,0 +1,29 @@
+<?php get_header('single'); ?>
+	
+	<div class="container-fluid">
+		<div class="row categoria">
+			<h1> FILMES </h1>
+		</div>
+	</div>
+
+	<div class="row categoria">
+	    <div class="col-md-8" id="conteudo"> <!--class bootstrap--><!--id="conteudo"-->
+	        <?php if (have_posts()) : while (have_posts()) : the_post(); ?> <!-- comandos do wp, para verificar se tem posts para mostrar--> 
+	        <div id="post-<?php the_ID(); ?>" <?php post_class("col-md-5"); ?>> <!-- cmd para identificar a categoria e criar a classe com o nome + a classe md-5 -->
+	            <div class="thumbnail"><?php the_post_thumbnail(); ?></div><!--img do post-->
+	                <h3> <a href="<?php the_permalink(); ?>"> <?php the_title(); ?> </a> </h3> <!-- Chama o titulo do post--><!-- permalink faz ele linkar com o post correspondente-->
+	                <p> <?php the_excerpt(); ?> </p> <!--the_content(); Chama o conteudo do post, ja o excerpt mostra o resumo-->
+	        </div>
+	        <?php endwhile; ?>    
+	        <?php else: ?>
+	        <?php endif; ?>
+	    </div> <!-- Fim div conteudo -->    
+	    <?php get_sidebar(); ?>
+	
+
+
+<?php get_sidebar('single'); ?>
+	</div> <!-- Fim row -->
+
+
+<?php get_footer(); ?>
